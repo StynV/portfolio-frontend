aea03fbccfc581cd0e8d9ac3df170f00c9d3abde
Styn Vercauteren <styn.vercauteren@outlook.com>
2021-11-21 22:42:10 +0100

    Add text align justify for the text

--- components/block.tsx
+++ components/block.tsx
@@ -16,7 +16,7 @@ export class Block extends React.Component<{
         <div className={this.props.class} style={{ backgroundColor: this.props.block.backgroundColor }}>
           <div>
               <h2 className="lg:text-3xl text-4xl">{ this.props.block.subtitle }</h2>
-              <p className="lg:text-xl text-2xl mt-5 max-w-xl">{ this.props.block.body }</p>
+              <p className="lg:text-xl text-2xl mt-5 max-w-xl text-justify">{ this.props.block.body }</p>
               <p className="lg:text-xl text-2xl mt-5 max-w-xl"><b>Duration:</b> { this.props.block.duration }</p>
               <BlockIcons icons={ this.props.block.icons } class="lg:h-10 lg:w-10  h-14 w-14  mt-5 mr-2 float-left" />
           </div>
