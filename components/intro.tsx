716b64b8d45224666a10c56d45d65ca04b2ceb8f
Styn Vercauteren <styn.vercauteren@outlook.com>
2021-11-21 21:30:11 +0100

    make intro bigger on phone

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,13 +9,13 @@ export class Intro extends React.Component<{intro: IntroData}> {
     return (
       <div className={styles.intro}>
         <div className={styles.introText}>
-            <h1 className="text-6xl">{ this.props.intro.title }</h1>
-            <h2 className="text-4xl mt-10">{ this.props.intro.subtitle }</h2>
-            <p className="text-2xl mt-5">{ this.props.intro.body }</p>
+            <h1 className="lg:text-6xl text-8xl">{ this.props.intro.title }</h1>
+            <h2 className="lg:text-4xl text-6xl mt-10">{ this.props.intro.subtitle }</h2>
+            <p className="lg:text-2xl text-4xl mt-5">{ this.props.intro.body }</p>
         </div>
         <div className="ml-auto">
           <Image
-            className="object-contain object-right rounded-lg"
+            className="object-contain lg:object-right rounded-lg"
             src={headShot}
             alt="headshot"
             width={400}
